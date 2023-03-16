d2a36c1d72bc60e03f4db66cd16aa1aa841494aa
benyaminrmb <benyaminrmb@gmail.com>
2023-03-16 07:21:28 +0330

    Fixed register user controller

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Enums\Role as EnumRole;
 use App\Http\Controllers\Controller;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
@@ -21,14 +23,19 @@ class RegisteredUserController extends Controller
     public function store(Request $request): Response
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', Rules\Password::defaults()],
         ]);
 
+        $role=Role::select('id')->where('name',EnumRole::UNVERIFIED_USER->value)->first();
+
         $user = User::create([
-            'name' => $request->name,
+            'first_name' => $request->first_name,
+            'last_name' => $request?->last_name,
             'email' => $request->email,
+            'role_id' => $role->id,
             'password' => Hash::make($request->password),
         ]);
 
