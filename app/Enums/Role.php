8ce6cca73f4c1d1fa6453d6b929deb97abe4f683
amir-ys <amir.you74@gmail.com>
2023-03-19 15:38:27 +0330

    fix user migration and role

--- app/Enums/Role.php
+++ app/Enums/Role.php
@@ -4,7 +4,7 @@ namespace App\Enums;
 
 enum Role: string
 {
-    case ADMIN = 'admin';
-    case VERIFIED_USER = 'verified_user';
     case UNVERIFIED_USER = 'unverified_user';
+    case VERIFIED_USER = 'verified_user';
+    case ADMIN = 'admin';
 }
