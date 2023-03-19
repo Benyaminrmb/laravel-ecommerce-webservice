5101162cb99331bb455dbad7caab96294e127832
Benyamin Rmb <benyaminrmb@gmail.com>
2023-03-20 00:05:01 +0330

    Added en lang resource

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'created'   => 'User has been created.',
+    'failed'   => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+];
