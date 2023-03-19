8df57c0e651b240ebd33d64673f3a1d815d3957b
Benyamin Rmb <benyaminrmb@gmail.com>
2023-03-20 00:05:36 +0330

    Added phone_number to user table and nullable email field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
 
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
+            $table->string('phone_number')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->foreignId('role_id')->default(\App\Enums\Role::UNVERIFIED_USER)->constrained()->cascadeOnDelete();
+            $table->timestamp('phone_number_verified_at')->nullable();
+            $table->foreignId('role_id')->constrained()->cascadeOnDelete();
             $table->string('password')->nullable();
 
             $table->rememberToken();
