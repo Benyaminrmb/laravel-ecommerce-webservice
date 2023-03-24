1c1be38e3d078dcfea4c7294516916938967d5e5
Benyamin Rmb <benyaminrmb@gmail.com>
2023-03-24 23:08:00 +0430

    Added children method in category.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public function children(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(related: Category::class, foreignKey: 'parent_id');
+    }
 }
