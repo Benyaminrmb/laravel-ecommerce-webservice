9902aed486693913b6ecb05ad7548a6ee32eebf6
Benyamin Rmb <benyaminrmb@gmail.com>
2023-03-24 23:10:54 +0430

    Created index and store method.

--- app/Http/Controllers/General/CategoryController.php
+++ app/Http/Controllers/General/CategoryController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\General;
 
+use App\Http\Controllers\Controller;
+use App\Http\Requests\General\CategoryRequest;
 use App\Interface\CategoryRepositoryInterface;
+use Symfony\Component\HttpFoundation\Response;
 
-class CategoryController
+class CategoryController extends Controller
 {
     private CategoryRepositoryInterface $categoryRepository;
     public function __construct(CategoryRepositoryInterface $categoryRepository)
@@ -12,8 +15,19 @@ class CategoryController
         $this->categoryRepository = $categoryRepository;
     }
 
-    public function getAll()
+    public function index()
     {
-        return $this->categoryRepository->getAll();
+        $categories=$this->categoryRepository->getAll();
+        return $this->jsonResponse(data:$categories);
     }
+
+    public function store(CategoryRequest $request)
+    {
+        $newCategory=[
+            'name'=>$request->get('name')
+        ];
+        $category=$this->categoryRepository->create($newCategory);
+        return $this->jsonResponse(data:$category,statusCode: Response::HTTP_CREATED);
+    }
+
 }
