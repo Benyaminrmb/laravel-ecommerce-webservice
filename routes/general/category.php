351c249c8de4066802cd73442d8975866affcf31
Benyamin Rmb <benyaminrmb@gmail.com>
2023-03-25 17:17:46 +0430

    Removed category route from service provider.

--- routes/general/category.php
+++ routes/general/category.php
@@ -3,6 +3,12 @@
 
 use App\Http\Controllers\General\CategoryController;
 
-Route::resource('/', CategoryController::class,['only' => ['index','show']]);
-Route::resource('/', CategoryController::class,['only' => ['store','update','destroy']])
-    ->middleware('auth:api');
+//Route::resource('/', CategoryController::class,['only' => ['index','show']]);
+Route::prefix('/category')->group(function () {
+    Route::get('/', [CategoryController::class, 'index']);
+    Route::middleware('auth:api')->group(function () {
+        Route::post('/', [CategoryController::class, 'store']);
+        Route::put('/{category}', [CategoryController::class, 'update']);
+        Route::put('/{category}/restore', [CategoryController::class, 'restore']);
+     });
+});
