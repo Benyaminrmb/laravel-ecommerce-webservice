8f10c973f4b4c4f4755e61a323d9033142b593a2
Benyamin Rmb <benyaminrmb@gmail.com>
2023-03-31 21:33:04 +0430

    Create upload route.

---
+++ routes/general/upload.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\General\BrandController;
+use App\Http\Controllers\General\UploadController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('/upload')->group(function () {
+    Route::middleware('auth:api')->group(function () {
+        Route::post('/', [UploadController::class, 'store']);
+    });
+});
